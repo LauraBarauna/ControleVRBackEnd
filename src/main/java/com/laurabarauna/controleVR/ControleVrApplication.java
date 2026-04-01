fbec41551afdb38b04e20b9ef552743fe5a0d777
Laura Barauna <laura.barauna@senior.com.br>
2026-04-01 17:42:01 -0300

    Inicia projeto, adicionando dependências e plugins no pom

---
+++ src/main/java/com/laurabarauna/controleVR/ControleVrApplication.java
@@ -0,0 +1,13 @@
+package com.laurabarauna.controleVR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleVrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleVrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/laurabarauna/controleVR/ControleVrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laurabarauna.controleVR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleVrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
